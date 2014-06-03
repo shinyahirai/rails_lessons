87d96ca9ad4c24fcbd3dc9771bfc8d5afc8078a9
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 22:07:07 +0900

    Finished lesson 21.

--- taskapp/app/models/task.rb
+++ taskapp/app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ActiveRecord::Base
-  belongs_to :project
+  belongs_to :project # model作成時のreferencesにより自動生成されたコード
 end
--- taskapp/config/routes.rb
+++ taskapp/config/routes.rb
@@ -1,6 +1,8 @@
 Taskapp::Application.routes.draw do
 
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
   root 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
