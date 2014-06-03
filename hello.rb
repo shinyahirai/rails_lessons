b24f6ff3ebac1fe61827f91dac1a5ca728b3784d
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 18:17:38 +0900

    finished lesson 4.

--- hello.rb
+++ hello.rb
@@ -1,5 +1,7 @@
 =begin
 
+レッスン 02
+
 * 哲学
 - DRY
 -- Don't Repeat Yourself # 同じようなものを2回書かない
@@ -10,5 +12,20 @@
 Model:データ
 View:画面
 Controller:ModelとViewをとりもつもの
+
+
+レッスン 03
+
+rails new myaap
+cd myapp
+rails server
+
+環境によっては
+vim Gemfile
+gem 'therubyracer', platforms: :ruby # コメントアウトを解除
+
+bundle install
+
+
 
 =end
