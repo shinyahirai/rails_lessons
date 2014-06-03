e53589b4711e354fd42d3cdd1dbed05ca2ea27ac
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 23:11:04 +0900

    Finished lesson 27.

--- taskapp/config/routes.rb
+++ taskapp/config/routes.rb
@@ -4,7 +4,7 @@ Taskapp::Application.routes.draw do
     resources :tasks, only: [:create, :destroy]
   end
 
-  post 'projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
 
   root 'projects#index'
 
