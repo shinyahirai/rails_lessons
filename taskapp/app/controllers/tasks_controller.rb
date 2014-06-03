7d42794460b41bfed68136d77a19799d57e7fb8c
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 22:22:06 +0900

    Finished lesson 23

--- taskapp/app/controllers/tasks_controller.rb
+++ taskapp/app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+
+	def create
+		@project = Project.find(params[:project_id])
+		@task = @project.tasks.create(task_params)
+		redirect_to project_path(@project.id)
+	end
+
+	def destroy
+	end
+
+	private
+
+		def task_params # ここで一度フィルタリングして上に渡す
+			params[:task].permit(:title)
+		end
 end
--- taskapp/app/models/task.rb
+++ taskapp/app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   belongs_to :project # model作成時のreferencesにより自動生成されたコード
+  validates :title, presence: true
 end
