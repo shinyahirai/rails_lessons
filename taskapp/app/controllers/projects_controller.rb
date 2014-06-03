3905d181cf2fe0b60176f14eb84090b1e9e27a39
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 19:48:51 +0900

    Finished lesson 13.

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -11,4 +11,17 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
+
+	def create
+		@project = Project.new(project_params)
+		@project.save
+		redirect_to projects_path
+	end
+
+	private
+
+		def project_params
+			params[:project].permit(:title)
+		end
+
 end
