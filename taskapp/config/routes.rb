e2cb5ad3c6b7758a4580a8fa0434fd1e0c3857e4
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 22:52:07 +0900

    Finished lesson 26.

--- taskapp/config/routes.rb
+++ taskapp/config/routes.rb
@@ -3,6 +3,9 @@ Taskapp::Application.routes.draw do
   resources :projects do
     resources :tasks, only: [:create, :destroy]
   end
+
+  post 'projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
   root 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
