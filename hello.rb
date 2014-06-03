90d65348e991ab4ab1bf1827b1fb93cc790dff23
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 18:07:31 +0900

    Finished lesson 2.

---
+++ hello.rb
@@ -0,0 +1,14 @@
+=begin
+
+* 哲学
+- DRY
+-- Don't Repeat Yourself # 同じようなものを2回書かない
+- CoC
+-- Convention over Configuration # 規約が多い
+
+* MVCアーキテクチャ
+Model:データ
+View:画面
+Controller:ModelとViewをとりもつもの
+
+=end
